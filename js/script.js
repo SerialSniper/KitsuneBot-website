62fabcd1eaa5d2930c8f0746c00464b0c7bd7cf4
unknown <mome.broili@gmail.com>
2021-02-25 20:38:21 +0100

    1.5

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+const path = "res/img/cores/";
+const cores = [
+    "wheatley",
+    "space",
+    "adventure",
+    "fact"
+];
+
+var changed = false;
+
+function updateImage() {
+    if($(".wheatley-easteregg > img").offset().top > $(window).scrollTop() + $(window).height()) {
+        if(!changed) {
+            let rand = Math.floor(Math.random() * 4);
+            $(".wheatley-easteregg > img").attr("src", `res/img/cores/${cores[rand]}.png`);
+            changed = true;
+        }
+    } else changed = false;
+}
+
+$(document).ready(updateImage);
+$(document).on("scroll", updateImage);
+
 $(document).on("mousemove", function(e) {
     var angle = 90 - Math.atan2(($(document).width() - e.pageX), ($(document).height() - e.pageY)) * (180 / Math.PI);
 
